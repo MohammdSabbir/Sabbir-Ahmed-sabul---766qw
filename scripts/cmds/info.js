d105ad64d0d9a52a934c5f32a5862e02c02a9d9e
mohammadsabbir898 <sabbirahmedsabul9@gmail.com>
2024-09-28 07:45:01 +0600

    Update info.js

--- scripts/cmds/info.js
+++ scripts/cmds/info.js
@@ -2,42 +2,41 @@ const fs = require('fs');
 const moment = require('moment-timezone');
 
 module.exports = {
-  config: {
-    name: "info",
-    aliases: ["info"],
-    version: "1.0",
-    author: "NEXXO",
-    countDown: 0,
-    role: 0,
-    shortDescription: { vi: "", en: "" },
-    longDescription: { vi: "", en: "" },
-    category: "owner",
-    guide: { en: "" },
-    envConfig: {}
-  },
-  onStart: async function ({ message }) {
-    const botName = "MIM";
-    const botPrefix = "/";
-    const authorName = "SABBIR AHMED";
-    const ownAge = "18";
-    const teamName = "🌚🫦";
-    const authorFB = "https://www.facebook.com/profile.php?id=100071882764076";
-    const authorInsta = "নেই";
-    const tikTok = "ছাপরি জিনিস ইউজ করি না 😎";
-    const urls = JSON.parse(fs.readFileSync('nexxo.json'));
-    const link = urls[Math.floor(Math.random() * urls.length)];
-    const now = moment().tz('Asia/Sylhet');
-    const date = now.format('MMMM Do YYYY');
-    const time = now.format('h:mm:ss A');
-    const uptime = process.uptime();
-    const seconds = Math.floor(uptime % 60);
-    const minutes = Math.floor((uptime / 60) % 60);
-    const hours = Math.floor((uptime / (60 * 60)) % 24);
-    const days = Math.floor(uptime / (60 * 60 * 24));
-    const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
+	config: {
+		name: "info",
+		version: "1.0",
+		author: "cliff",
+		countDown: 20,
+		role: 0,
+		shortDescription: { vi: "", en: "" },
+		longDescription: { vi: "", en: "" },
+		category: "owner",
+		guide: { en: "" },
+		envConfig: {}
+	},
+	onStart: async function ({ message }) {
+		const botName = "MIM";
+		const botPrefix = "/";
+		const authorName = "SABBIR AHMED";
+		const ownAge = "18+";
+		const teamName = "🌚🫦";
+		const authorFB = "https://www.facebook.com/profile.php?id=100071882764076";
+		const authorInsta = "sabbir232";
+		const tikTok = "এই গুলো ব্যবহার করি না🌚🫦";
+		const urls = JSON.parse(fs.readFileSync('Gabyu.json'));
+		const link = urls[Math.floor(Math.random() * urls.length)];
+		const now = moment().tz('Asia/Sylhet');
+		const date = now.format('MMMM Do YYYY');
+		const time = now.format('h:mm:ss A');
+		const uptime = process.uptime();
+		const seconds = Math.floor(uptime % 60);
+		const minutes = Math.floor((uptime / 60) % 60);
+		const hours = Math.floor((uptime / (60 * 60)) % 24);
+		const days = Math.floor(uptime / (60 * 60 * 24));
+		const uptimeString = `${days} days ${hours} hours ${minutes} minutes ${seconds} seconds`;
 
-    message.reply({
-      body: `《  Bot & Owner Info 》
+		message.reply({
+			body: `《  Bot & Owner Info 》
 \Name: ${botName}
 \Bot Prefix: ${botPrefix}
 \owner: ${authorName}
@@ -50,12 +49,12 @@ module.exports = {
 \Team: ${teamName}
 \Uptime: ${uptimeString}
 \===============`,
-      attachment: await global.utils.getStreamFromURL(link)
-    });
-  },
-  onChat: async function ({ event, message, getLang }) {
-    if (event.body && event.body.toLowerCase() === "info2") {
-      this.onStart({ message });
-    }
-  }
+			attachment: await global.utils.getStreamFromURL(link)
+		});
+	},
+	onChat: async function ({ event, message, getLang }) {
+		if (event.body && event.body.toLowerCase() === "info") {
+			this.onStart({ message });
+		}
+	}
 };
